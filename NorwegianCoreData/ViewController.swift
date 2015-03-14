707a604efaf9dbe9f9a751804fbe18cc2745b95b
dcromp <dcromp@live.co.uk>
2015-03-14 21:30:10 +0100

    Demo app to load norwegian to english translations

--- NorwegianCoreData/ViewController.swift
+++ NorwegianCoreData/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFied: UITextField!
+    @IBOutlet weak var translationLabel: UILabel!
+    
+    @IBAction func translateButton(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
